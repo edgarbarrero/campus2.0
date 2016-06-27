ff43228bfcbf6e3fc544a55f02b5027adaacc692
edgarmtp <edgar@mytwinplace.com>
2016-06-28 00:05:09 +0200

    sign up form css

---
+++ db/migrate/20160627214723_add_colums_user.rb
@@ -0,0 +1,10 @@
+class AddColumsUser < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :surname, :string
+    add_column :users, :dni, :string
+    add_column :users, :phone, :integer
+    add_column :users, :birth_date, :string
+    add_column :users, :date, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160626085435) do
+ActiveRecord::Schema.define(version: 20160627214723) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,12 @@ ActiveRecord::Schema.define(version: 20160626085435) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.string   "surname"
+    t.string   "dni"
+    t.integer  "phone"
+    t.string   "birth_date"
+    t.string   "date"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
