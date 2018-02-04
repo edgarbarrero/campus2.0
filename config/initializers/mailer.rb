ed97cb149a6ee3409a162d5855d24038d7a17090
edgarbarrero <edgar@ideas4all.com>
2018-02-04 14:05:53 +0100

    changes in mailer and confirmation mail

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                 587,
   domain:               'rcdasociacion.es',
-  user_name:            'tutoria@rcdasociacion.es',
+  user_name:            'tutorias@rcdasociacion.es',
   password:             Rails.application.secrets.email_password,
   authentication:       :plain,
   enable_starttls_auto: true
