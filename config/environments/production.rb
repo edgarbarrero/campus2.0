1a19cda4c9de8998037e3d6b16a25d73f9a78555
edgarbarrero <edgar@ideas4all.com>
2018-02-08 22:42:22 +0100

    config for mailer in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -65,7 +65,8 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { host: 'campus-online.herokuapp.com' }
-
+  config.action_controller.asset_host = 'https://campus.rcdasociacion.es'
+  config.action_mailer.asset_host = config.action_controller.asset_host
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
