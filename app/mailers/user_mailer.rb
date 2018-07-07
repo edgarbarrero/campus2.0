a8faae1a7bcf9a665806cd568db7eeb6a21c1d7d
edgarbarrero <edgar@ideas4all.com>
2018-07-07 22:30:50 +0200

    changes for https://trello.com/c/zPvD5DiI/91-tareas-pendientes-mayo-18

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,11 +3,11 @@ class UserMailer < ApplicationMailer
 
   def new_user_registration(user)
     @user = user
-    mail(to: 'pablogonzalez@rcdasociacion.es', cc: 'edgar.barrero.mateo@gmail.com', subject: 'Un nuevo alumno ha hecho el pago')
+    mail(to: 'pablogonzalez@rcdasociacion.es', cc: ['edgar.barrero.mateo@gmail.com', 'reciclaje@rcdasociacion.es'], subject: 'Un nuevo alumno ha hecho el pago')
   end
 
   def user_complete_course(user)
     @user = user
-    mail(to: 'pablogonzalez@rcdasociacion.es', cc: 'edgar.barrero.mateo@gmail.com', subject: 'Un alumno ha completado el curso')
+    mail(to: 'pablogonzalez@rcdasociacion.es', cc: ['edgar.barrero.mateo@gmail.com', 'reciclaje@rcdasociacion.es'], subject: 'Un alumno ha completado el curso')
   end
 end
